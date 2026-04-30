51f2308fbf061d109741237ed7271ae9beeea743
gomes0609 <leonardogomes0609@gmail.com>
2026-04-29 22:11:22 -0300

    meu primeiro código

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Olá mundo")
+let nomeHeroi = "Leo"
+let xp = 5000
+
+if(xp >= 2000){
+    nivel="Prata";
+}else if (xp >= 4000) {
+    nivel="Ouro";
+}else if (xp >= 5000){
+    nivel="Ametista";
+}else xp <= 900
+
+console.log(`O heroi de nome ${nomeHeroi} está no nivel ${nivel}`)
