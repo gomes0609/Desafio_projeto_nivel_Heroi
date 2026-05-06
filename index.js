3b326426c96628d5bcc80c78c3edc7e670c2ad4c
gomes0609 <leonardogomes0609@gmail.com>
2026-05-06 20:10:38 -0300

    Adição de mais níveis

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let nomeHeroi = "Leo"
-let xp = 5000
+let nomeHeroi = "Leo";
+let xp = 5000;
+let nivel;
 
-if(xp >= 2000){
+if(xp >= 1000){
     nivel="Prata";
-}else if (xp >= 4000) {
+}else if (xp >= 4000){
     nivel="Ouro";
 }else if (xp >= 5000){
     nivel="Ametista";
-}else xp <= 900
+}else if (xp >=7000){
+    nivel="Platina";
+}else if (xp >= 8000){
+    nivel="Ascendente";
+}else if (xp >= 9000){
+    nivel="Imortal";
+} else {
+    nivel="Radiante";
+}
 
-console.log(`O heroi de nome ${nomeHeroi} está no nivel ${nivel}`)
+console.log(`O heroi de nome ${nomeHeroi} está no nivel ${nivel}`);
